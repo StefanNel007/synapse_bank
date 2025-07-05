db8da249c5d7284efb6d28cb0a56d164031d787f
StefanNel007 <stefan_nel@hotmail.com>
2025-07-05 17:13:09 +0200

    cors for dev server set

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,34 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                const attrs = ['data-new-gr-c-s-check-loaded', 'data-gr-ext-installed'];
+                const remove = el => attrs.forEach(a => el.removeAttribute(a));
+                remove(document.documentElement);
+                if (document.body) remove(document.body);
+
+                // Watch for re-injection of Grammarly attrs on html/body
+                const observer = new MutationObserver(muts => {
+                  muts.forEach(m => {
+                    if (m.type === 'attributes' && attrs.includes(m.attributeName)) {
+                      const t = m.target;
+                      if (t instanceof HTMLElement && (t.tagName === 'HTML' || t.tagName === 'BODY')) {
+                        t.removeAttribute(m.attributeName);
+                      }
+                    }
+                  });
+                });
+                observer.observe(document.documentElement, { attributes: true });
+                if (document.body) observer.observe(document.body, { attributes: true });
+              })();
+            `,
+          }}
+        />
+      </head>
       <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>
         {children}
       </body>
